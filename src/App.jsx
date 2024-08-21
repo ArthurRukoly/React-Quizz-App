f15642c928d7324a4e3e8c15e526a3189a034755
Arturs Rukols <arthurrukoly000@gmail.com>
2024-08-21 10:10:56 +0300

    logo created

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-function App() {}
+import Logo from "./components/Logo";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Logo />
+      </header>
+      <main></main>
+    </>
+  );
+}
 
 export default App;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import LogoImg from "../assets/quiz-logo.png";
+
+export default function Logo() {
+  return (
+    <>
+      <img src={LogoImg} alt="Logo Image" />
+      <h1>REACTQUIZ</h1>
+    </>
+  );
+}
